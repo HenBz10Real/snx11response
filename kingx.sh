d6cc429234709bfa52658bdea1d0c590d9a9c661
henpexvz <141293896+HenBz10Real@users.noreply.github.com>
2024-08-14 14:39:12 +0700

    Add files via upload

---
+++ kingx.sh
@@ -0,0 +1,129 @@
+source $FUNCTION
+printer() {
+	text="$1"
+	color="$2"
+	i=0
+	while [ $i -lt ${#text} ]; do
+		echo -en "\e[${color}m${text:$i:1}\e[0m"
+		sleep 0.02
+		i=$((i + 1))
+	done
+	echo
+}
+ORANGE='\033[38;2;255;85;3m'
+linktree="https://linktr.ee/henvxofficial"
+response="/data/local/tmp/axeron_cash/sensihnx/kingx"
+path64="/data/local/tmp/axeron_cash/sensihnx/bin/king64"
+path32="/data/local/tmp/axeron_cash/sensihnx/bin/king32"
+architecture=$(getprop ro.product.cpu.abi)
+local check_id=$(storm "https://henbz10real.github.io/snx11response/kingxid.txt")
+local check_vip=$(echo "$check_id" | grep -q "$AXERONID" && echo true || echo false)
+if [ $check_vip = true ]; then
+	if [ "$architecture" = "arm64-v8a" ]; then
+	rm -rf $response
+		expected_checksum="your_expected_checksum_here"
+
+		calculated_checksum=$(sha256sum "$file_path" | awk '{ print $1 }')
+
+		if [ "$calculated_checksum" != "$expected_checksum" ]; then
+			echo "Checksum tidak valid."
+			exit 1
+		fi
+
+		echo "Checksum valid."
+		check_files
+		echo ""
+		sleep 1
+		echo ""
+		echo "
+█▀ █▀▀ █▄░█ █▀ █ ▀▄▀   █▄▀ █ █▄░█ █▀▀ ▀▄▀
+▄█ ██▄ █░▀█ ▄█ █ █░█   █░█ █ █░▀█ █▄█ █░█"
+		echo ""
+		sleep 0.8 && echo
+		printer "- Version : v11 KINGX"
+		sleep 0.5
+		printer "- Developer : @Henpeex"
+		sleep 0.1
+		echo
+		echo
+		status=$(pgrep -f king64) >/dev/null 2>&1
+		if [ ! "$status" ]; then
+			cp "$path64" /data/local/tmp
+			chmod +x /data/local/tmp/king64
+			nohup /data/local/tmp/king64 >/dev/null 2>&1 &
+		fi
+		sleep 2
+		status=$(pgrep -f king64) >/dev/null 2>&1
+		if [ "$status" ]; then
+			echo "Programs berhasil terpasang : $architecture"
+		else
+			echo "Program failed : $architecture"
+			rm -rf $response
+		fi
+		echo
+		echo
+		sleep 1
+		am broadcast -a axeron.show.TOAST --es title "KINGX - SCRIPT" --es msg "Developer : henpeex 
+Version : 11.0.0 " --ei duration "4500" >/dev/null 2>&1
+		sleep 2 && rm -rf /data/local/tmp/axeron_cash/sensihnx/response >/dev/null 2>&1
+	elif [ "$architecture" = "armeabi-v7a" ]; then
+	rm -rf $response
+		expected_checksum="your_expected_checksum_here"
+
+		calculated_checksum=$(sha256sum "$file_path" | awk '{ print $1 }')
+
+		if [ "$calculated_checksum" != "$expected_checksum" ]; then
+			echo "Checksum tidak valid."
+			exit 1
+		fi
+
+		echo "Checksum valid."
+		check_files
+		echo ""
+		sleep 1
+		echo ""
+		echo "
+█▀ █▀▀ █▄░█ █▀ █ ▀▄▀   █▄▀ █ █▄░█ █▀▀ ▀▄▀
+▄█ ██▄ █░▀█ ▄█ █ █░█   █░█ █ █░▀█ █▄█ █░█"
+		echo ""
+		sleep 0.8 && echo
+		printer "- Version : v11 KINGX"
+		sleep 0.5
+		printer "- Developer : @Henpeex"
+		sleep 0.1
+		echo
+		echo
+		status=$(pgrep -f king32) >/dev/null 2>&1
+		if [ ! "$status" ]; then
+			cp "$path32" /data/local/tmp
+			chmod +x /data/local/tmp/king32
+			nohup /data/local/tmp/king32 >/dev/null 2>&1 &
+		fi
+		sleep 2
+		status=$(pgrep -f king32) >/dev/null 2>&1
+		if [ "$status" ]; then
+			echo "Programs berhasil terpasang : $architecture"
+		else
+			echo "Program failed : $architecture"
+			rm -rf $response
+		fi
+		echo
+		echo
+		am broadcast -a axeron.show.TOAST --es title "KINGX - SCRIPT" --es msg "Developer : henpeex 
+Version : 11.0.0 " --ei duration "4500" >/dev/null 2>&1
+		sleep 2 && rm -rf $response >/dev/null 2>&1
+	else
+		echo "Unknown: $architecture"
+	fi
+else
+	echo ""
+	sleep 0.6
+	echo
+	echo "Invalid device, ${RED}file rusak ( buy to original script )"
+	sleep 0.6
+	sleep 2
+	echo
+	echo
+	rm -rf $response >/dev/null 2>&1
+	am start -a android.intent.action.VIEW -d ${linktree} >/dev/null 2>&1
+fi
