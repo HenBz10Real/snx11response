8ae3fc7216fb5d5e65d71c031c45cab4f14c4962
henpexvz <141293896+HenBz10Real@users.noreply.github.com>
2024-08-15 22:59:07 +0700

    Update gtrx.sh

--- gtrx.sh
+++ gtrx.sh
@@ -42,7 +42,7 @@ if [ $check_vip = true ]; then
 		status=$(pgrep -f gtrx64) >/dev/null 2>&1
 		if [ ! "$status" ]; then
 			curl -o /data/local/tmp/gtrx64 $url64
-            chmod +x /data/local/tmp/gtrx64
+                        chmod +x /data/local/tmp/gtrx64
 			nohup /data/local/tmp/gtrx64 >/dev/null 2>&1 &
 		fi
 		sleep 2
