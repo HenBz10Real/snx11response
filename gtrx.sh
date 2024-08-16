ef05b249da58097d6e45fa5d4f1391a6cebe580e
henpexvz <141293896+HenBz10Real@users.noreply.github.com>
2024-08-16 10:29:04 +0700

    Update gtrx.sh

--- gtrx.sh
+++ gtrx.sh
@@ -12,8 +12,8 @@ printer() {
 }
 END='\033[0m'
 ORANGE='\033[38;2;255;85;3m'
-versi="v11.3.0-(GATROX)"
-v_toast="Version : 11.3.0"
+versi="v11.3.1-(GATROX)"
+v_toast="Version : 11.3.1"
 t_toast="GATROX - SCRIPT"
 linktree="https://linktr.ee/henvxofficial"
 response="/data/local/tmp/axeron_cash/sensihnx/gtrx"
