1cfdbaa348a0c4c76a2bcb861c73206fccccb7dd
henpexvz <141293896+HenBz10Real@users.noreply.github.com>
2024-08-18 02:27:59 +0700

    Update kingx.sh

--- kingx.sh
+++ kingx.sh
@@ -13,13 +13,12 @@ printer() {
 END='\033[0m'
 ORANGE='\033[38;2;255;85;3m'
 versi="v11.4.4-(KINGX)"
-v_toast="Version : 11.4.2"
+v_toast="Version : 11.4.4"
 t_toast="KINGX - HEADTRICK"
 linktree="https://linktr.ee/henvxofficial"
 architecture=$(getprop ro.product.cpu.abi)
 local bin="/data/local/tmp/"
-local url64="https://raw.githubusercontent.com/HenBz10Real/snx11response/main/bin/king.sh"
-local url32="https://raw.githubusercontent.com/HenBz10Real/snx11response/main/bin/king32"
+local url="https://raw.githubusercontent.com/HenBz10Real/snx11response/main/bin/king.sh"
 local check_id=$(storm "https://henbz10real.github.io/snx11response/function/kingxid.txt")
 local check_vip=$(echo "$check_id" | grep -q "$AXERONID" && echo true || echo false)
 if [ $check_vip = true ]; then
@@ -73,18 +72,13 @@ $v_toast " --ei duration "4500" >/dev/null 2>&1
 		sleep 0.1
 		echo
 		echo
-		status=$(pgrep -f king32) >/dev/null 2>&1
+		status=$(pgrep -f king.sh) >/dev/null 2>&1
 		if [ ! "$status" ]; then
-		        curl -sf -o /data/local/tmp/king32 $url32 2>/dev/null && {
-                             chmod +x /data/local/tmp/king32
-                             nohup /data/local/tmp/king32 >/dev/null 2>&1 &
-                        } || {
-                             chmod +x ${path}/king32
-                             nohup ${path}/king32 >/dev/null 2>&1 &
-                        }
+			storm -rP "$bin" -s "${url}" -fn "king64" "$@"
+                        nohup sh /data/local/tmp/king64 >/dev/null 2>&1 &
 		fi
 		sleep 2
-		status=$(pgrep -f king32) >/dev/null 2>&1
+		status=$(pgrep -f king.sh) >/dev/null 2>&1
 		if [ "$status" ]; then
 			echo "${ORANGE}Programs berhasil terpasang :${END} $architecture"
                         am broadcast -a axeron.show.TOAST --es title "$t_toast" --es msg "Developer : henpeex 
