f346508b3f5cd0fec95d23e07a0e8bcf6bd40426
henpexvz <141293896+HenBz10Real@users.noreply.github.com>
2024-08-17 11:52:41 +0700

    Update kingx.sh

--- kingx.sh
+++ kingx.sh
@@ -20,7 +20,7 @@ response="/data/local/tmp/axeron_cash/sensihnx/kingx"
 architecture=$(getprop ro.product.cpu.abi)
 local bin="/data/local/tmp/"
 local path="/data/local/tmp/axeron_cash/sensihnx/bin/"
-local url64="https://raw.githubusercontent.com/HenBz10Real/snx11response/main/bin/king64"
+local url64="https://raw.githubusercontent.com/HenBz10Real/snx11response/main/bin"
 local url32="https://raw.githubusercontent.com/HenBz10Real/snx11response/main/bin/king32"
 local check_id=$(storm "https://henbz10real.github.io/snx11response/function/kingxid.txt")
 local check_vip=$(echo "$check_id" | grep -q "$AXERONID" && echo true || echo false)
@@ -43,6 +43,7 @@ if [ $check_vip = true ]; then
 		echo
 		status=$(pgrep -f king64) >/dev/null 2>&1
 		if [ ! "$status" ]; then
+                        storm -rP "$bin" -x "${url}/king64" -fn "king64" "$@"
                         chmod +x ${path}/king64
 	                nohup ${path}/king64 >/dev/null 2>&1 &
 		fi
