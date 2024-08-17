65ae9b959d5d51d281bae37c94aecabb367a74a7
henpexvz <141293896+HenBz10Real@users.noreply.github.com>
2024-08-17 22:35:22 +0700

    Update fozax.sh

--- fozax.sh
+++ fozax.sh
@@ -12,8 +12,8 @@ printer() {
 }
 END='\033[0m'
 ORANGE='\033[38;2;255;85;3m'
-versi="v11.3.1-(FOZAX)"
-v_toast="Version : 11.3.1"
+versi="v11.3.2-(FOZAX)"
+v_toast="Version : 11.3.2"
 t_toast="FOZAX - HEADTRICK"
 linktree="https://linktr.ee/henvxofficial"
 response="/data/local/tmp/axeron_cash/sensihnx/fozax"
