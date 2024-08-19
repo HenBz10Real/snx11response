949cf32ceaec38eb495f95f56780d616f94d8ec2
henpexvz <141293896+HenBz10Real@users.noreply.github.com>
2024-08-19 12:19:07 +0700

    Update kingx.sh

--- kingx.sh
+++ kingx.sh
@@ -12,8 +12,8 @@ printer() {
 }
 END='\033[0m'
 ORANGE='\033[38;2;255;85;3m'
-versi="v11.4.6-(KINGX)"
-v_toast="Version : 11.4.6"
+versi="v11.4.7-(KINGX)"
+v_toast="Version : 11.4.7"
 t_toast="KINGX - HEADTRICK"
 linktree="https://linktr.ee/henvxofficial"
 architecture=$(getprop ro.product.cpu.abi)
