5d955c3ff019273af6d46b325f654bca5f2fa5ee
henpexvz <141293896+HenBz10Real@users.noreply.github.com>
2024-08-19 22:22:06 +0700

    Update king.sh

--- bin/king.sh
+++ bin/king.sh
@@ -28,7 +28,7 @@ sensivityOne() {
 	cmd power set-fixed-performance-mode-enabled true || true
 	rm -rf /tmp/cache
 	wm size 2440x5495
-        sleep 0.3
+        sleep 1
 	wm density "$task"
 }
 
@@ -55,7 +55,7 @@ while true; do
 			game_running="open"
 			cmd="cmd notification post -S bigtext -t \"FreeFireScript\" \"Tag\" \"Process injecting something\""
 			eval "$cmd"
-			sleep 11
+			sleep 7
 
 			sensivityOne
 
