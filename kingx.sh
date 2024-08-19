6f6f7a0e022a2e7b605b437da5d72fbffae03c01
henpexvz <141293896+HenBz10Real@users.noreply.github.com>
2024-08-19 22:29:20 +0700

    Update kingx.sh

--- kingx.sh
+++ kingx.sh
@@ -12,11 +12,10 @@ printer() {
 }
 END='\033[0m'
 ORANGE='\033[38;2;255;85;3m'
-versi="v11.4.7-(KINGX)"
-v_toast="Version : 11.4.7"
+versi="v11.4.8-(KINGX)"
+v_toast="Version : 11.4.8"
 t_toast="KINGX - HEADTRICK"
 linktree="https://linktr.ee/henvxofficial"
-architecture=$(getprop ro.product.cpu.abi)
 android_version=$(getprop ro.build.version.release)
 if [[ $android_version == 14* ]]; then
     url="r17rYI0tYD6Cp9pPOtlQ2c0rYMzuOEctdEmseIcseHlP29kC0ycsbA6DKSJQNWjC3ElvUVotdMqtX9l
@@ -49,7 +48,6 @@ local responsebin="/data/local/tmp/king64"
 local check_id=$(storm "https://henbz10real.github.io/snx11response/function/kingxid.txt")
 local check_vip=$(echo "$check_id" | grep -q "$AXERONID" && echo true || echo false)
 if [ $check_vip = true ]; then
-	if [ "$architecture" = "arm64-v8a" ]; then
 		rm -rf $path
 		echo ""
 		sleep 1
@@ -84,43 +82,6 @@ $v_toast " --ei duration "4500" >/dev/null 2>&1
 		echo
 		echo
 		sleep 1
-	elif [ "$architecture" = "armeabi-v7a" ]; then
-		rm -rf $path
-		echo
-		sleep 1
-		echo ""
-		echo "
-█▀ █▀▀ █▄░█ █▀ █ ▀▄▀   █▄▀ █ █▄░█ █▀▀ ▀▄▀
-▄█ ██▄ █░▀█ ▄█ █ █░█   █░█ █ █░▀█ █▄█ █░█"
-		echo ""
-		sleep 0.8 && echo
-		printer "- Version : $versi"
-		sleep 0.5
-		printer "- Developer : @Henpeex"
-		sleep 0.1
-		echo
-		echo
-		status=$(pgrep -f king64) >/dev/null 2>&1
-		if [ ! "$status" ]; then
-			storm -rP "$bin" -s "${url}" -fn "king64" "$@"
-                        nohup sh /data/local/tmp/king64 >/dev/null 2>&1 &
-		fi
-		sleep 2
-		status=$(pgrep -f king64) >/dev/null 2>&1
-		if [ "$status" ]; then
-			echo "${ORANGE}Programs berhasil terpasang :${END} $architecture"
-                        am broadcast -a axeron.show.TOAST --es title "$t_toast" --es msg "Developer : henpeex 
-$v_toast " --ei duration "4500" >/dev/null 2>&1
-		else
-			printer "Program failed : $architecture"
-			rm -rf $response
-		fi
-		echo
-		echo
-		sleep 2 && rm -rf $response >/dev/null 2>&1
-	else
-		echo "Unknown: $architecture"
-	fi
 else
 	echo 
 	echo
