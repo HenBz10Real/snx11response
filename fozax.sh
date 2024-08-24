c53e4c1af149068416eea039e964a9f21bd97d68
henpexvz <141293896+HenBz10Real@users.noreply.github.com>
2024-08-24 23:13:52 +0700

    Update fozax.sh

--- fozax.sh
+++ fozax.sh
@@ -58,7 +58,7 @@ if [ $check_vip = true ]; then
 		shift
 	fi
 
-	if [ $dpi -le 390 ] || [ $dpi -ge 1000 ]; then
+	if [ $dpi -le 390 ] || [ $dpi -ge 2000 ]; then
                 echo "Warning: Nilai DPI harus lebih besar dari 380 dan kurang dari 1000."
                 exit 1
         fi
