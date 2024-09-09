23ec9979912689e69e11a74350661a66fe3a6b88
henpexvz <141293896+HenBz10Real@users.noreply.github.com>
2024-09-09 20:36:41 +0700

    Update gtrx.sh

--- gtrx.sh
+++ gtrx.sh
@@ -63,7 +63,7 @@ if [ $check_vip = true ]; then
 		shift
 	fi
 
-	if [ $dpi -le 380 ] || [ $dpi -ge 1000 ]; then
+	if [ $dpi -le 380 ] || [ $dpi -ge 1400 ]; then
                 echo "Warning: Nilai DPI harus lebih besar dari 380 dan kurang dari 1000."
                 exit 1
         fi
